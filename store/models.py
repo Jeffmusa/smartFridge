71c4a278baa2214d3439bb962f16f2bb5ddcf29a
vonmutinda <maxwellwambua@gmail.com>
2018-10-30 14:58:43 +0300

    models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Vegetable (models.Model):
+    name = models.CharField(max_length = 50 )
+    thumbnail = models.ImageField(upload_to='product/vegetables' , default='')
+    price = models.PositiveIntegerField()
+
+
+class Cereal (models.Model):
+    name = models.CharField(max_length=50)
+    thumbnail = models.ImageField(upload_to='product/cereals', default='')
+    price = models.PositiveIntegerField()
+
+
+class Fruit (models.Model):
+    name = models.CharField(max_length=50)
+    thumbnail = models.ImageField(upload_to='product/fruits', default='')
+    price = models.PositiveIntegerField()
